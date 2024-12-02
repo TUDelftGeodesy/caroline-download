aa2b275b0da7234288f1a2435863e06b98ae682c
Niels Jansen <n.h.jansen@tudelft.nl>
2024-12-02 14:57:43 +0100

    Change name of logger

--- src/caroline_download/download.py
+++ src/caroline_download/download.py
@@ -7,10 +7,10 @@ import logging
 # imports us, if they want logging.
 # For more information, see:
 # https://docs.python.org/3/howto/logging.html#configuring-logging-for-a-library
-CAROLINE_DOWNLOAD_LOGGER = logging.getLogger(__name__)
-CAROLINE_DOWNLOAD_LOGGER.addHandler(logging.NullHandler())
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
 
 
 def download(download_config, geo_search=None, product_search=None):
     print(__name__)
-    CAROLINE_DOWNLOAD_LOGGER.info('Starting download')
+    logger.info('Starting download')
